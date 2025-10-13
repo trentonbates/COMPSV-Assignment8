33b8a3e8c9e468e209cae88c1f869231cc5f984b
trentonbates <batestrenton@outlook.com>
2025-10-13 18:51:24 -0500

    started assignment

--- hash_table.py
+++ hash_table.py
@@ -6,7 +6,12 @@ class Contact:
         number (str): The phone number of the contact.
     '''
     
-    pass # Delete this line when implementing the class
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
+
+    def __str__(self):
+        return self.name + ': ' + self.number 
 
 class Node:
     '''
@@ -16,8 +21,11 @@ class Node:
         value (Contact): The value (Contact object) associated with the key.
         next (Node): Pointer to the next node in case of a collision.
     '''
-   
-    pass # Delete this line when implementing the class
+    
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
 
 class HashTable:
     '''
@@ -32,6 +40,56 @@ class HashTable:
         print_table(): Prints the structure of the hash table.
     '''
     
-    pass # Delete this line when implementing the class
+    def __init__(self, size):
+        self.size = size
+        self.data = [None] * size
 
-# Test your hash table implementation here.  
+    def hash_function(self, key):
+        total = 0
+        for char in key:
+            total += ord(char)
+        return total % self.size
+    
+    def insert(self, key, value):
+        index = self.hash_function(key)
+
+        current = self.data[index]
+
+        if self.data[index] is None:
+            self.data[index] = Node(key, value)
+            return
+        
+        while current:
+            if current.key == key:
+                current.value = value
+                return
+            
+            if current.next is None:
+                break
+
+            current = current.next
+
+        current.next = Node(key, value)
+
+    def search(self, key):
+        index = self.hash_function(key)
+        current = self.data[index]
+
+        while current:
+            if current.key == key:
+                return current.value
+            current = current.next
+
+        return None
+
+    def print_table():
+        ...
+
+contact_1 = Contact("Riley", "123-456-7890")
+print(contact_1) # Riley: 123-456-7890
+
+contact_1 = Contact("Riley", "123-456-7890")
+node_1 = Node(contact_1.name, contact_1)
+print(node_1.key) # Riley 
+print(node_1.value) # Riley: 123-456-7890 
+print(node_1.next) # None
